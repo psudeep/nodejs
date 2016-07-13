8d0356ad895c00859291270f80446120fb53bd23
prashant_sudeep <prashant.sudeep@medimojo.in>
2016-07-13 13:45:49 +0530

    NodeJS Starter repo.. Used to build Web Apps and REST APIs.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+/*
+@Company: Medimojo
+@Author: Prashant Sudeep
+*/
+
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NodeStarter' });
+});
+
+module.exports = router;
